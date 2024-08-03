9512458646949d15b720aa9603b0a1d56a474882
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2024-08-04 00:07:20 +0700

    fix login auth guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,6 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
-    console.log(user);
     return {
       access_token: this.jwtService.sign(payload),
       user: user,
