ed5b1c8351a1ba4095770e5d4fcee45c95173f56
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-09 16:42:46 +0700

    fix update/remove object with code

--- src/modules/curriculums/curriculums.controller.ts
+++ src/modules/curriculums/curriculums.controller.ts
@@ -29,80 +29,19 @@ export class CurriculumsController {
     return this.curriculumsService.findAll(pag);
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.curriculumsService.findOne(+id);
-  // }
-
   @Get(':code')
   findOneByCode(@Param('code') code: string) {
     return this.curriculumsService.findOneByCode(code);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCurriculumDto: UpdateCurriculumDto,
-  ) {
-    return this.curriculumsService.update(+id, updateCurriculumDto);
+  @Patch(':code')
+  update(@Body() updateCurriculumDto: UpdateCurriculumDto) {
+    return this.curriculumsService.update(updateCurriculumDto);
   }
 
-  // @Patch(':id/subjects')
-  // async addSubject(
-  //   @Param('id') id: string,
-  //   @Body() createSubjectDtos: CreateSubjectDto[],
-  // ) {
-  //   console.log('Received data:', createSubjectDtos); // Log the received data
-
-  //   // Ensure createSubjectDtos is an array
-  //   if (!Array.isArray(createSubjectDtos)) {
-  //     throw new BadRequestException('Invalid data format: Expected an array');
-  //   }
-
-  //   // Validate each item in the array
-  //   createSubjectDtos.forEach((dto) => {
-  //     if (typeof dto.id !== 'string' || dto.id.trim() === '') {
-  //       throw new BadRequestException('Invalid subject name format');
-  //     }
-  //   });
-
-  //   const subjects = await Promise.all(
-  //     createSubjectDtos.map((dto) => this.subjectsService.create(dto)),
-  //   );
-
-  //   await Promise.all(
-  //     subjects.map(() => this.curriculumsService.addSubject(id, subjects)),
-  //   );
-
-  //   return this.curriculumsService.findOne(id); // add subject to curriculum
-  // }
-
-  // @Patch(':id/selectSubjects')
-  // async selectSubject(
-  //   @Param('id') id: string,
-  //   @Body() createSubjectDtos: CreateSubjectDto[],
-  // ) {
-  //   // option and select the former subjects
-  //   const subjects = await Promise.all(
-  //     createSubjectDtos.map((dto) => this.subjectsService.findOne(dto.id)),
-  //   );
-  //   return this.curriculumsService.selectSubject(id, subjects); // add subject to curriculum
-  // }
-
-  // @Patch(':id/coordinators')
-  // async addCoordinator(@Param('id') id: string, @Body() dto: number[]) {
-  //   return this.curriculumsService.addCoordinator(id, dto); // Add teacher to curriculum
-  // }
-
-  // @Patch(':id/plos')
-  // async addPLO(@Param('id') id: string, @Body() createPloDto: CreatePloDto) {
-  //   const plo = await this.plosService.create(createPloDto); // create plo to database
-  //   return this.curriculumsService.addPLO(id, plo); // add plo to curriculum
-  // }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.curriculumsService.remove(+id);
+  @Delete(':code')
+  remove(@Param('code') code: string) {
+    return this.curriculumsService.remove(code);
   }
 
   @Get('filters/:branchId')
