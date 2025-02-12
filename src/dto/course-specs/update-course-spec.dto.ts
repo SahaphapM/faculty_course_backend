c49020ad4d0955b3141fe6382fa0d0417c405a05
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-12 17:25:11 +0700

    update validation

--- src/dto/course-specs/update-course-spec.dto.ts
+++ src/dto/course-specs/update-course-spec.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCourseSpecDto } from './create-course-spec.dto';
+import { IsNumber, IsOptional } from 'class-validator';
 
-export class UpdateCourseSpecDto extends PartialType(CreateCourseSpecDto) {}
+export class UpdateCourseSpecDto extends PartialType(CreateCourseSpecDto) {
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  @IsOptional()
+  id: number;
+}
