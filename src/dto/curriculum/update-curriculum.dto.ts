b54aebcfa5682ba3da8f88af55f296fa96c76b96
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-12 18:58:43 +0700

    turn old create curr back

--- src/dto/curriculum/update-curriculum.dto.ts
+++ src/dto/curriculum/update-curriculum.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCurriculumDto } from './create-curriculum.dto';
+import { IsNumber } from 'class-validator';
 
-export class UpdateCurriculumDto extends PartialType(CreateCurriculumDto) {}
+export class UpdateCurriculumDto extends PartialType(CreateCurriculumDto) {
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  id: number;
+}
