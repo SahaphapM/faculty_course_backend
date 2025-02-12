89f5ab6ade8392fc9b42c3ca83aee3841ec4594e
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-12 17:39:23 +0700

    split method create/update

--- src/dto/course-specs/update-course-spec.dto.ts
+++ src/dto/course-specs/update-course-spec.dto.ts
@@ -1,9 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCourseSpecDto } from './create-course-spec.dto';
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsNumber } from 'class-validator';
 
 export class UpdateCourseSpecDto extends PartialType(CreateCourseSpecDto) {
   @IsNumber({ allowNaN: false, allowInfinity: false })
-  @IsOptional()
   id: number;
 }
