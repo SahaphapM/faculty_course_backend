43ba3f758e1f9577b9abcc8c8747070a5927bc5f
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-04-15 17:54:27 +0700

    remove unused file, add start.sh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'BUU-API', // Give your application a descriptive name
-      script: './dist/src/main.js', // Path to your built application's entry point
-      cwd: './', // Current working directory (usually the project root)
-      instances: 1, // Number of instances to run (for load balancing, usually equals to number of cores)
-      autorestart: true, // Automatically restart if the application crashes
-      watch: false, // Set to true if you want PM2 to restart on file changes (use with caution in production)
-      // env: {  // Environment variables
-      //   NODE_ENV: "production"
-      // },
-      // env_development: { // Environment variables for development
-      //   NODE_ENV: "development"
-      // }
+      name: 'BUU-API',
+      script: './start.sh', // Point to a shell script
+      cwd: './',
+      instances: 1,
+      autorestart: true,
+      watch: false,
     },
   ],
 };
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+npm i
+npm run build
+node ./dist/src/main.js
