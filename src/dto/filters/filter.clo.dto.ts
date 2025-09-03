685340ad321fa8a62c01e6a0f0d0b4ac93d1b656
SahaphapM <65160309@go.buu.ac.th>
2025-09-03 15:56:15 +0700

    fix: subject get clo subjectId query string

--- src/dto/filters/filter.clo.dto.ts
+++ src/dto/filters/filter.clo.dto.ts
@@ -1,10 +1,15 @@
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsOptional } from 'class-validator';
 import { BaseFilterParams } from './filter.base.dto';
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class CloFilterDto extends BaseFilterParams {
-  @ApiPropertyOptional({ description: 'Filter by subject id', required: false, example: 1 })
+  @ApiPropertyOptional({
+    description: 'Filter by subject id',
+    required: false,
+    example: 1,
+  })
   @IsOptional()
-  @IsNumber()
+  @Type(() => Number)
   subjectId?: number;
 }
